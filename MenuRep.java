29cf5f346ecb12c83874fe5fa85757f861eefba9
Maxwell Noffsinger <mcnoffsinger@my.waketech.edu>
2025-09-29 09:15:50 -0400

    Actually did the assignment this time.

--- MenuRep.java
+++ MenuRep.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
+import java.util.Date;
+
 
 public class MenuRep {
 
     public static Scanner scanner = new Scanner(System.in);
+
+    
+    
+    //calendar.setTime(currentDate)
 
     public static int getIntInput(String prompt) {
         System.out.print(prompt);
@@ -11,31 +17,28 @@ public class MenuRep {
     }
     
     public static void main(String[] args) {
+        Date currentDate = new Date();
         int input;
         do{
-            System.out.println("This is a menu. \nPress 1 to make text gray\nPress 2 to make text blue\nPress 3 to make text green\nPress 4 to make text pink\nPress 5 to exit menu");
+            System.out.println("This is a menu. \n1 = say hello\n2 = display current time\n3 = exit");
             input = getIntInput("\t--> ");
             switch(input){
                 case 1:
-                System.out.println("\u001B[30m");//So I looked this up
+                System.out.println("Hello");//Sorry I did random stuff last time
                 break;
                 case 2:
-                System.out.println("\u001B[34m");//They're basically escape characters like \n or \t
+                System.out.println(currentDate);//I feel bad
                 break;
                 case 3:
-                System.out.println("\u001B[32m");//They use ANSI color codes
+                System.out.println("Exiting menu");
                 break;
-                case 4:
-                System.out.println("\u001B[35m");//This wasn't ChatGPT
-                break;
-                case 5:
-                System.out.println("closing menu");
+                
 
                 default:
                 break;
 
             }
 
-        }while(input != 5);
+        }while(input != 3);
     }
 }
